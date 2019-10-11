47c402109c60c51170cd07ff1bf87fe71713fe2f
senthilbalaji.g <senthilbalaji.ganesan@hashedin.com>
2019-10-11 22:59:27 +0530

    changed tslint config

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,7 @@
+import { Request, Response } from "express";
+
 export class UserController {
-  public static getUser(req, res) {
+  public static getUser(req: Request, res: Response) {
     try {
       throw new Error("Some error to check maps");
       res.send({ user_id: req.params.id });
